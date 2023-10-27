6f403e3cecd3e12c226f32d254d0e91ee03355d6
Zumrud03 <nusretovazumrud@gmail.com>
2023-10-27 11:06:46 +0400

    16-20 task

---
+++ assets/script.js
@@ -0,0 +1,73 @@
+// ? 16
+
+// function task16(num) {
+//   let arr = [];
+//   for (let i = 0; i <= num; i++) {
+//     if (num % i == 0) {
+//       arr.push(i);
+//     }
+//   }
+//   console.log(arr);
+// }
+// task16(6);
+
+// ? 17
+
+// function task17(num) {
+//   count = 0;
+
+//   for (let i = 0; i <= num; i++) {
+//     if (num % i == 0) {
+//       count = count + 1;
+//     }
+//   }
+//   console.log(count);
+// }
+// task17(12);
+
+// ? 18
+
+// function task18(num) {
+//   sum = 0;
+//   hasil = 1;
+
+//   for (let i = 0; i <= num; i++) {
+//     if (num % i == 0) {
+//       sum += +i;
+//       hasil *= +i;
+//     }
+//   }
+//   console.log("cemi = " + sum, "hasili = " + hasil);
+// }
+// task18(6);
+
+// ? 20
+
+// function sade_eded(num) {
+//   for (let i = 2; i < num; i++) {
+//     if (num % i === 0) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+// }
+// console.log(sade_eded(32));
+
+// ? 21 unfinished
+
+// function sade_eded(num1, num2) {
+//   // let num
+//   for (let i = num1; i < num2; i++) {
+//     for (let j = 2; j < num1; j++) {
+//       if (num1 % j === 0) {
+//         return true;
+//       }
+
+//       //   return false
+//       // }
+//     }
+//   }
+// }
+
+// console.log(sade_eded(3, 9));
